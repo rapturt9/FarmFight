3ff145cf352fba4541cb72693e1b7277788a77c8
XtraSuccess <xtrasccs@gmail.com>
2021-09-13 13:06:19 -0400

    Tile setups

---
+++ FarmFightUnity/Assets/Scripts/EngineFiles/tile.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/tiles", order = 1)]
+public class tile : Tile
+{
+     
+}
