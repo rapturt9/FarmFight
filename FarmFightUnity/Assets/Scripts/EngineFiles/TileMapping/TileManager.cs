2b8929e6fb84c1ae39a52fa8485d0bd99a38db06
XtraSuccess <xtrasccs@gmail.com>
2021-09-19 15:29:18 -0400

    explanations

--- FarmFightUnity/Assets/Scripts/EngineFiles/TileMapping/TileManager.cs
+++ FarmFightUnity/Assets/Scripts/EngineFiles/TileMapping/TileManager.cs
@@ -50,6 +50,11 @@ public class TileManager : MonoBehaviour
         
     }
 
+    /// <summary>
+    /// convert screen coords to hex coords
+    /// </summary>
+    /// <param name="screenPos"></param>
+    /// <returns></returns>
     public Hex ScreenToHex(Vector3Int screenPos )
     {
         Vector3 Point = Camera.main.ScreenToWorldPoint(screenPos);
@@ -57,21 +62,58 @@ public class TileManager : MonoBehaviour
         return Hex.fromWorld(Point);
     }
 
+    /// <summary>
+    /// convert viewport coords to hex coords
+    /// </summary>
+    /// <param name="viewPos"></param>
+    /// <returns></returns>
     public Hex ViewPortToHex(Vector3 viewPos)
     {
         return Hex.fromWorld(Camera.main.ViewportToWorldPoint(viewPos));
     }
 
+    /// <summary>
+    /// convert world coords to hex coords
+    /// </summary>
+    /// <param name="worldPos"></param>
+    /// <returns></returns>
     public Hex WorldToHex(Vector3 worldPos)
     {
         return Hex.fromWorld(worldPos);
     }
 
+    /// <summary>
+    /// Move the hex 
+    /// </summary>
+    /// <param name="current"></param>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <returns></returns>
     public Hex Move(Hex current,int x, int y)
     {
         return current +  (Hex.right * x) + (Hex.up * y);
     }
 
 
-    
+    /// <summary>
+    /// use to get a tile on a certain tile map
+    /// </summary>
+    /// <param name="mapName"></param>
+    /// <param name="coord"></param>
+    /// <returns></returns>
+    public TileTemp GetTile(string mapName, Hex coord)
+    {
+        return this[mapName][coord];
+    }
+
+    /// <summary>
+    /// use to set a tile on a certain tile map
+    /// </summary>
+    /// <param name="mapName"></param>
+    /// <param name="coord"></param>
+    /// <param name="tile"></param>
+    public void setTile(string mapName, Hex coord, TileTemp tile)
+    {
+        this[mapName][coord] = tile;
+    }
 }
