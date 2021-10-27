6fcfc2455f239d1f8378729fba95d15c30b8dc43
Eli-bassoon <61474669+Eli-bassoon@users.noreply.github.com>
2021-10-27 04:38:31 -0400

    Color changing clouds

--- FarmFightUnity/Assets/Scripts/EngineFiles/BattleCloud.cs
+++ FarmFightUnity/Assets/Scripts/EngineFiles/BattleCloud.cs
@@ -9,9 +9,15 @@ public class BattleCloud : NetworkBehaviour
 {
     public TileHandler handler;
 
+    const float tint = 0.2f;
+    private Hex hexCoord;
+    private NetworkVariable<Color> color = new NetworkVariable<Color>();
+
+
     private void Start()
     {
         handler = TileManager.TM["Crops"];
+        StartCoroutine("RefreshColor");
     }
 
     // Add to all client tiles
@@ -45,5 +51,54 @@ public class BattleCloud : NetworkBehaviour
     {
         Hex coord = BoardHelperFns.ArrayToHex(coordArray);
         handler[coord].battleCloud = gameObject;
+        hexCoord = coord;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    // Gets the percentage each team has of soldier health
+    float[] getSoldierHealthFractions()
+    {
+        float[] soldierHealths = new float[Repository.maxPlayers];
+        float[] soldierHealthFractions = new float[Repository.maxPlayers];
+        float totalHealth = 0;
+
+        // Get combined health for each time
+        foreach (var soldier in handler[hexCoord].getSoldierEnumerator())
+        {
+            soldierHealths[soldier.owner.Value] += soldier.Health.Value;
+            totalHealth += soldier.Health.Value;
+        }
+        // Rescale
+        for (int playerId = 0; playerId<Repository.maxPlayers; playerId++)
+        {
+            soldierHealthFractions[playerId] = soldierHealths[playerId] / totalHealth;
+        }
+        return soldierHealthFractions;
+    }
+
+    Color getColor()
+    {
+        Color newColor = new Color();
+        float[] fractions = getSoldierHealthFractions();
+        for (int playerId = 0; playerId<Repository.maxPlayers; playerId++)
+        {
+            newColor += OutlineSetter.OS.TeamColors[playerId] * fractions[playerId];
+        }
+        // Make it look a bit more pastel
+        newColor += Color.white * tint;
+        return newColor;
+    }
+
+    private IEnumerator RefreshColor()
+    {
+        while (true)
+        {
+            GetComponent<SpriteRenderer>().color = getColor();
+            yield return new WaitForSeconds(.05f);
+        }
     }
 }
